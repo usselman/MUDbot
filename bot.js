cec05003827572d12e44f195fae61451368d27b9
usselman <sarkar.shirish@gmail.com>
2024-02-29 13:57:41 -0800

    initial deployment to heroku

--- bot.js
+++ bot.js
@@ -28,8 +28,7 @@ client.once('ready', () => {
 	console.log('Ready!');
 	//send i'm online! message in campfire
 	const channel = client.channels.cache.get('1022424180454588416');
-	channel.send('I live! ' + Date()
-		+ '! \nCheck <#1010833300396457984> for my commands.');
+	channel.send('I live! \nCheck <#1010833300396457984> for my commands.');
 	client.user.setActivity('Standing by...');
 
 });
